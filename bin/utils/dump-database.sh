50412998597fc0f2bd3c99c8cfce28d45f894c31
J. Iván Alegre <IvanAlegre@users.noreply.github.com>
2017-03-27 18:14:53 +0200

    Move some files and fix gohan dev

---
+++ bin/utils/dump-database.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+BACKUP_NAME="backup-"`date +%Y%m%d`
+COLLECTIONS=(blacklist campaign customcategories email reporting seedtag_new studio user)
+SERVER=mongodb1-instance-2
+
+ssh $SERVER "rm -Rf $BACKUP_NAME"
+
+for collection in "${COLLECTIONS[@]}"; do
+    ssh $SERVER "mongodump --gzip -d $collection -o $BACKUP_NAME"
+done
+
+ssh $SERVER "mongodump --gzip -d analytics -c PublisherReportData -o $BACKUP_NAME"
+
+echo "Transfering backup"
+rsync --progress -a $SERVER:$BACKUP_NAME initial-data
+
+echo "Done! Saved as $BACKUP_NAME"
