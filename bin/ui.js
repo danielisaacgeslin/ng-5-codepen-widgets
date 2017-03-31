9f4dd3e14ddd16e80923679e5807b5e2ff926ab7
J. Iván Alegre <IvanAlegre@users.noreply.github.com>
2017-03-31 10:51:47 +0200

    Now everything in one screen, some bugfixes

--- bin/ui.js
+++ bin/ui.js
@@ -2,13 +2,13 @@
 
 const blessed = require('blessed');
 const services = require('./services');
+const childProcess = require('child_process');
 
 const screen = blessed.screen({ smartCSR: true });
 screen.title = 'seedtag CLI';
 
 const box = blessed.box({ content: '{center}seedtag CLI{/}', tags: true });
 
-/* Sync part */
 const syncForm = blessed.form({
   top: 'center',
   left: 'center',
@@ -20,8 +20,6 @@ const syncForm = blessed.form({
   border: { type: 'line' },
 });
 
-let checkboxes;
-
 const selectAll = blessed.checkbox({
   text: 'Select all',
   parent: syncForm,
@@ -30,81 +28,29 @@ const selectAll = blessed.checkbox({
   mouse: true
 });
 
-// screen.exec('du', ['-a', '/Users/jivan/tmp']);
-selectAll.on('check', obj => {
-  checkboxes.forEach(c => c.check());
-  screen.render();
-});
-
-selectAll.on('uncheck', obj => {
-  checkboxes.forEach(c => c.uncheck());
-  screen.render();
-});
-
-
-checkboxes = Object.keys(services).map((s, index) => blessed.checkbox({
+const checkboxes = Object.keys(services).map((s, index) => blessed.checkbox({
   text: s,
   parent: syncForm,
   top: index + 1,
   left: 2,
-  mouse: true
+  mouse: true,
+  style: {
+    focus: { fg: 'black', bg: 'white' },
+    hover: { fg: 'black', bg: 'white' }
+  }
 }));
-
-const confirm = blessed.button({
-  content: 'Sync',
-  parent: syncForm,
-  bottom: 1,
-  shrink: true,
-  keys: true,
-  border: { type: 'line' },
-  left: 3,
-  shadow: true,
-  mouse: true,
-  style: {
-    focus: { fg: 'black', bg: 'white' },
-    hover: { fg: 'black', bg: 'white' }
-  }
-});
-
-const cancel = blessed.button({
-  content: 'Cancel',
-  parent: syncForm,
-  bottom: 1,
-  shrink: true,
-  keys: true,
-  border: { type: 'line' },
-  left: 10,
-  shadow: true,
-  mouse: true,
-  style: {
-    focus: { fg: 'black', bg: 'white' },
-    hover: { fg: 'black', bg: 'white' }
-  }
-});
-
-screen.append(box);
-
-/* MAIN PART */
-const mainForm = blessed.form({
-  // parent: box,
-  top: 'center',
-  left: 'center',
-  height: '90%',
-  width: '90%',
-  // content: 'Hello {bold}seedtag{/bold}',
-  tags: true,
-  keys: true,
-  border: { type: 'line' },
-});
 
 const actions = [
   {
-    name: 'Sync',
+    name: 'Sync selected services',
     callback: (cb) => {
-      box.remove(mainForm);
-      box.append(syncForm);
-      syncForm.focus();
-      screen.render();
+      const servicesToSync = checkboxes.filter(c => c.value).map(c => c.text);
+      if (servicesToSync.length === 0) {
+        box.setContent('You have to select at least one service');
+        return screen.render();
+      }
+      screen.destroy();
+      return childProcess.spawn('st', ['sync'].concat(servicesToSync), { stdio: 'inherit' });
     },
   },
   {
@@ -122,21 +68,37 @@ const actions = [
       console.log('Restoring today\'s db');
       screen.exec('st', ['db_restore'], {}, () => process.exit());
     }
+  },
+  {
+    name: 'Exit (q/Esc/Ctrl+c)',
+    callback: (cb) => {
+      console.log('Ciao!');
+      process.exit();
+    }
   }
 ];
 
+selectAll.on('check', obj => {
+  checkboxes.forEach(c => c.check());
+  screen.render();
+});
+
+selectAll.on('uncheck', obj => {
+  checkboxes.forEach(c => c.uncheck());
+  screen.render();
+});
 
 actions.map((a, i) => {
   const button = blessed.button({
     content: a.name,
-    parent: mainForm,
-    top: i * 3,
-    width: 15,
+    parent: syncForm,
+    bottom: 2,
+    width: 21,
     height: 3,
-    // shrink: true,
+    shrink: true,
     keys: true,
     border: { type: 'line' },
-    left: 4,
+    left: i * 24,
     shadow: true,
     mouse: true,
     style: {
@@ -148,26 +110,9 @@ actions.map((a, i) => {
   return button;
 });
 
-box.append(mainForm);
-mainForm.focus();
-
-confirm.on('press', () => {
-  const servicesToSync = checkboxes.filter(c => c.value).map(c => c.text);
-  if (servicesToSync.length === 0) {
-    box.setContent('You have to select at least one service');
-    return screen.render();
-  }
-  screen.destroy();
-  return screen.exec('st', ['sync'].concat(servicesToSync), {}, () => process.exit());
-});
-
-cancel.on('press', () => {
-  box.remove(syncForm);
-  box.append(mainForm);
-  mainForm.focus();
-  screen.render();
-});
-
+screen.append(box);
+box.append(syncForm);
+syncForm.focus();
 screen.render();
 
 // Quit on Escape, q, or Control-C.
