49bac6ff55341ad5e89d132805f63c9c825d5eb6
David Muñoz <david@gul.es>
2017-04-06 09:49:17 +0200

    Updated tier script

--- bin/st-plugins/tiers.js
+++ bin/st-plugins/tiers.js
@@ -3,7 +3,7 @@ const Repository = require('../utils/Repository');
 
 function run(cmd, args) {
   console.log(cmd, args);
-  childProcess.spawnSync(cmd, args, { stdio: 'inherit' });
+  childProcess.spawn(cmd, args, { stdio: 'inherit' });
 }
 
 function findTier(n) {
@@ -12,7 +12,7 @@ function findTier(n) {
   for (const repo of repos) {
     repo.services
       .filter(svc => svc.tier === n)
-      .forEach(svc => servicesInTier.push(svc.name));
+      .forEach(svc => servicesInTier.push(svc));
   }
   return servicesInTier;
 }
@@ -22,12 +22,23 @@ const tier = {
   description: 'Starts tier <n>',
   action: (n) => {
     n = parseInt(n, 10);
-    let servicesToRun = [];
+    let services = [];
     if (n === 0) {
-      servicesToRun = servicesToRun.concat(['kafka', 'zookeeper', 'mongo', 'redis']);
+      run('/bin/bash', ['-c', `docker-compose up -d ${Repository.baseServices.join(' ')}`]);
+      services = services.concat(Repository.baseServices);
     }
-    servicesToRun = servicesToRun.concat(findTier(n));
-    run('/bin/bash', ['-c', `docker-compose restart ${servicesToRun.join(' ')}`]);
+    findTier(n)
+      .forEach(svc => {
+        const args = [];
+        if (svc.dcFile) {
+          args.push(`-f ${svc.dcFile}`);
+        }
+        args.push(`up -d ${svc.name}`);
+        run('/bin/bash', ['-c', `sleep 30 ; docker-compose ${args.join(' ')}`]);
+        services.push(svc.name);
+      });
+
+    run('/bin/bash', ['-i', '-c', `sleep 60 ; docker-compose logs -f --tail=100 ${services.join(' ')}`]);
   }
 };
 
