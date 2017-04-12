b0bb5535af803677e3828fa4fef17a210254d871
J. Iván Alegre <IvanAlegre@users.noreply.github.com>
2017-04-12 18:42:17 +0200

    touch now does it in container

--- bin/st-t.js
+++ bin/st-t.js
@@ -3,12 +3,10 @@ const childProcess = require('child_process');
 const program = require('commander');
 const guess = require('./utils/guess-service');
 
-const execOpts = options.vexecOpts;
-
 program
   .parse(process.argv);
 
 const service = program.args[0] || guess();
-childProcess.spawn('/bin/bash', ['-i', '-c',
-  `"find ${service} -maxdepth 2 ! -path '*node_modules/*' -name *.js | head -n 1 | \
-xargs touch"`], execOpts);
+childProcess.spawn('st', ['sh', service,
+  '\'bash -c "find . -maxdepth 2 ! -path \\"*node_modules/*\\" -name *.js | head -n 1 | xargs touch"\''],
+  options.vexecOpts);
