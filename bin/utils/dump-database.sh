b5e22752eebfc6478ec55860e222e2c4f3d51b6d
Jose Manuel Pereira <jm.pereira.g@gmail.com>
2017-08-23 15:15:47 +0200

    Update st dump script

--- bin/utils/dump-database.sh
+++ bin/utils/dump-database.sh
@@ -4,15 +4,15 @@ BACKUP_NAME="backup-"`date +%Y%m%d`
 COLLECTIONS=(blacklist campaign customcategories email reporting seedtag_new studio user audit)
 SERVER=mongodb1-instance-2
 
-ssh $SERVER "rm -Rf $BACKUP_NAME"
+gcloud compute ssh $SERVER -- "rm -Rf $BACKUP_NAME"
 
 for collection in "${COLLECTIONS[@]}"; do
-    ssh $SERVER "mongodump --gzip -d $collection -o $BACKUP_NAME"
+  gcloud compute ssh $SERVER -- "mongodump --gzip -d $collection -o $BACKUP_NAME"
 done
 
 # ssh $SERVER "mongodump --gzip -d analytics -c PublisherReportData -o $BACKUP_NAME"
 
 echo "Transfering backup"
-rsync --progress -a $SERVER:$BACKUP_NAME initial-data
+gcloud compute scp $SERVER:$BACKUP_NAME initial-data --recurse
 
 echo "Done! Saved as $BACKUP_NAME"
