c1f887a32be08cf5c635d0a40fc61181eebb7b0d
danielisaacgeslin <danielisaacgeslin@gmail.com>
2017-10-13 20:30:25 +0200

    deleting out of place folder

--- poc-angular-spa/packages/poc-angular-spa/src/app/states/user/user.queries.ts
+++
@@ -1,9 +0,0 @@
-import { createSelector } from 'reselect';
-
-import { AppState } from '../';
-import { UserState } from './user.state';
-import { User } from '../../entities/User';
-
-export const getUserState = (state: AppState): UserState => state.user;
-
-export const getUsers = createSelector(getUserState, (state: UserState) => state.users);
--- poc-angular-spa/packages/poc-angular-spa/src/app/states/user/user.state.ts
+++
@@ -1,9 +0,0 @@
-import { User } from '../../entities/User';
-
-export interface IUserState {
-    users: User[];
-}
-
-export class UserState implements IUserState {
-    public users: User[] = [new User({ name: 'dan', age: 15 })];
-}
