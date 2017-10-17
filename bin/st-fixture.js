98c165caa7cff56ac69db719df3835ab1250ef21
J. Iván Alegre <IvanAlegre@users.noreply.github.com>
2017-10-17 12:30:07 +0200

    Load fixtures in container

--- bin/st-fixture.js
+++ bin/st-fixture.js
@@ -3,7 +3,9 @@ const childProcess = require('child_process');
 const program = require('commander');
 const fs = require('fs');
 
-const FIXTURES_PATH = `${options.cwd}/initial-data`;
+const FIXTURES_FOLDER = 'initial-data';
+
+const FIXTURES_PATH = `${options.cwd}/${FIXTURES_FOLDER}`;
 
 program
   .option('-a, --all', 'Load all available fixtures')
@@ -18,8 +20,8 @@ const availableFixtures = getAvailableFixtures();
 
 function loadFixture(name) {
   console.log('Loading fixture', name);
-  const file = `${FIXTURES_PATH}/${name}.js`;
-  childProcess.spawn('mongo', [name, file], options.vexecOpts);
+  const file = `${FIXTURES_FOLDER}/${name}.js`;
+  childProcess.spawn('docker-compose', ['exec', 'mongo', 'mongo', name, file], options.vexecOpts);
 }
 
 const names = program.all ? availableFixtures : program.args;
